8b5c4189d87b74c239b181ce6fb22e1a938e2ffd
dragan <dragovanmarkic@gmail.com>
2018-12-05 00:13:07 +0100

    Initial express server with routes files

---
+++ routes/api/profile.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+
+// @route        GET api/profile/test
+// @description  Tests profile route
+// @access       Public
+router.get('/test', (req, res) => res.json({
+  msg: "profile Works"
+}));
+
+module.exports = router;
